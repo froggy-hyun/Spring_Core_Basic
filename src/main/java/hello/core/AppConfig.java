a94d71bbbe1bc5fb7a89bc42620b04c131beb915
1233day <1233day@naver.com>
2023-03-02 13:58:32 +0900

    38강

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+//    스프링 컨테이너가 각각 @Bean을 호출해서 스프링 빈을 생성한다. 그래서 memberRepository() 는
+//    다음과 같이 총 3번이 호출되어야 하는 것 아닐까?
+//    1. 스프링 컨테이너가 스프링 빈에 등록하기 위해 @Bean이 붙어있는 memberRepository() 호출
+//    2. memberService() 로직에서 memberRepository() 호출
+//    3. orderService() 로직에서 memberRepository() 호출
+//    그런데 출력 결과는 모두 1번만 호출된다.
+//    call AppConfig.memberService
+//    call AppConfig.memberRepository
+//    call AppConfig.orderService
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
